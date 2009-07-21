6626e982cbce1239207159bdcb5f96b4be7a7fae
Tim Schaub <tim.schaub@gmail.com>
2009-07-21 04:06:48 +0000

    Docs for attribute store.

--- src/script/data/AttributesStore.js
+++ src/script/data/AttributesStore.js
@@ -12,38 +12,35 @@
 
 /** api: (define)
  *  module = gxp.data
- *  class = WMSCapabilitiesStore
+ *  class = AttributesStore
  *  base_link = `Ext.data.DataStore <http://extjs.com/deploy/dev/docs/?class=Ext.data.DataStore>`_
  */
 Ext.namespace("gxp.data");
 
-/**
- * Class: gxp.data.AttributesStore
- * Small helper class to make creating stores for remotely-loaded attributes
- *     data easier. AttributesStore is pre-configured with a built-in
- *     {Ext.data.HttpProxy} and {gxp.data.AttributesReader}.  The HttpProxy
- *     is configured to allow caching (disableCaching: false) and uses GET.
- *     If you require some other proxy/reader combination then you'll have to
- *     configure this with your own proxy or create a basic Ext.data.Store
- *     and configure as needed.
- *
- * Extends: Ext.data.Store
+/** api: constructor
+ *  .. class:: AttributesStore(config)
+ *  
+ *      Small helper class to make creating stores for remotely-loaded attributes
+ *      data easier. AttributesStore is pre-configured with a built-in
+ *      ``Ext.data.HttpProxy`` and :class:`gxp.data.AttributesReader`.  The
+ *      HttpProxy is configured to allow caching (disableCaching: false) and
+ *      uses GET. If you require some other proxy/reader combination then you'll
+ *      have to configure this with your own proxy or create a basic
+ *      ``Ext.data.Store`` and configure as needed.
  */
 
-/**
- * Constructor: gxp.data.AttributesStore
- * Create a new attributes store object.
- *
- * Parameters:
- * config - {Object} Store configuration.
- *
- * Configuration options:
- * format - {OpenLayers.Format} A parser for transforming the XHR response into
- *     an array of objects representing attributes.  Defaults to an
- *     {OpenLayers.Format.WFSDescribeFeatureType} parser.
- * fields - {Array | Function} Either an Array of field definition objects as
- *     passed to Ext.data.Record.create, or a Record constructor created using
- *     Ext.data.Record.create.  Defaults to ["name", "type"]. 
+/** api: config[format]
+ *  ``OpenLayers.Format``
+ *  A parser for transforming the XHR response into an array of objects
+ *  representing attributes.  Defaults to an
+ *  ``OpenLayers.Format.WFSDescribeFeatureType`` parser.
+ */
+
+/** api: config[fields]
+ *  ``Array or Function``
+ *  Either an array of field definition objects as passed to
+ *  ``Ext.data.Record.create``, or a record constructor created using
+ *  ``Ext.data.Record.create``.  Defaults to ``["name", "type"]``. 
  */
 gxp.data.AttributesStore = function(c) {
     gxp.data.AttributesStore.superclass.constructor.call(
