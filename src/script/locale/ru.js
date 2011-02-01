61ed5c5804641dccf7782cfaf83bfdef005384e7
Юджин <lazareveugene@eugene-laptop.(none)>
2011-02-01 22:04:33 +0500

    added ru.js file

---
+++ src/script/locale/ru.js
@@ -0,0 +1,250 @@
+
+GeoExt.Lang.add("ru", {
+
+    "gxp.plugins.AddLayers.prototype": {
+        addMenuText: "Add layers",
+        addActionTip: "Add layers",
+        addServerText: "Add a New Server",
+        untitledText: "Untitled",
+        addLayerSourceErrorText: "Error getting WMS capabilities ({msg}).\nPlease check the url and try again.",
+        availableLayersText: "Available Layers",
+        doneText: "Done"
+    },
+    
+    "gxp.plugins.BingSource.prototype": {
+        title: "Bing Layers",
+        roadTitle: "Bing Roads",
+        aerialTitle: "Bing Aerial",
+        labeledAerialTitle: "Bing Aerial With Labels"
+    },    
+
+    "gxp.plugins.FeatureEditor.prototype": {
+        createFeatureActionTip: "Create a new feature",
+        editFeatureActionTip: "Edit existing feature"
+    },
+    
+    "gxp.plugins.FeatureGrid.prototype": {
+        displayFeatureText: "Display on map",
+        firstPageTip: "First page",
+        previousPageTip: "Previous page",
+        zoomPageExtentTip: "Zoom to page extent",
+        nextPageTip: "Next page",
+        nextPageTip: "Last page"
+    },
+    
+    "gxp.plugins.GoogleSource.prototype": {
+        title: "Google Layers",
+        roadmapAbstract: "Show street map",
+        satelliteAbstract: "Show satellite imagery",
+        hybridAbstract: "Show imagery with street names",
+        terrainAbstract: "Show street map with terrain"
+    },
+
+    "gxp.plugins.LayerProperties.prototype": {
+        menuText: "Layer Properties",
+        toolTip: "Layer Properties"
+    },
+    
+    "gxp.plugins.LayerTree.prototype": {
+        rootNodeText: "Layers",
+        overlayNodeText: "Overlays",
+        baseNodeText: "Base Layers"
+    },
+
+    "gxp.plugins.Legend.prototype": {
+        menuText: "Show Legend",
+        tooltip: "Show Legend"
+    },
+
+    "gxp.plugins.Measure.prototype": {
+        lengthMenuText: "Length",
+        areaMenuText: "Area",
+        lengthTooltip: "Measure length",
+        areaTooltip: "Measure area",
+        measureTooltip: "Measure"
+    },
+
+    "gxp.plugins.Navigation.prototype": {
+        menuText: "Pan Map",
+        tooltip: "Pan Map"
+    },
+
+    "gxp.plugins.NavigationHistory.prototype": {
+        previousMenuText: "Zoom To Previous Extent",
+        nextMenuText: "Zoom To Next Extent",
+        previousTooltip: "Zoom To Previous Extent",
+        nextTooltip: "Zoom To Next Extent"
+    },
+
+    "gxp.plugins.OSMSource.prototype": {
+        title: "OpenStreetMap Layers",
+        mapnikAttribution: "Data CC-By-SA by <a href='http://openstreetmap.org/'>OpenStreetMap</a>",
+        osmarenderAttribution: "Data CC-By-SA by <a href='http://openstreetmap.org/'>OpenStreetMap</a>"
+    },
+
+    "gxp.plugins.MapQuestSource.prototype": {
+        title: "MapQuest Layers",
+        osmAttribution: "Tiles Courtesy of <a href='http://open.mapquest.co.uk/' target='_blank'>MapQuest</a> <img src='http://developer.mapquest.com/content/osm/mq_logo.png' border='0'>",
+        osmTitle: "MapQuest OpenStreetMap",
+        naipAttribution: "Tiles Courtesy of <a href='http://open.mapquest.co.uk/' target='_blank'>MapQuest</a> <img src='http://developer.mapquest.com/content/osm/mq_logo.png' border='0'>",
+        naipTitle: "MapQuest Imagery"
+    },
+
+    "gxp.plugins.QueryForm.prototype": {
+        queryActionText: "Query",
+        queryMenuText: "Query layer",
+        queryActionTip: "Query the selected layer",
+        queryByLocationText: "Query by location",
+        currentTextText: "Current extent",
+        queryByAttributesText: "Query by attributes",
+        queryMsg: "Querying...",
+        cancelButtonText: "Cancel",
+        noFeaturesTitle: "No Match",
+        noFeaturesMessage: "Your query did not return any results."
+    },
+
+    "gxp.plugins.RemoveLayer.prototype": {
+        removeMenuText: "Remove layer",
+        removeActionTip: "Remove layer"
+    },
+
+    "gxp.plugins.WMSGetFeatureInfo.prototype": {
+        infoActionTip: "Get Feature Info",
+        popupTitle: "Feature Info"
+    },
+
+    "gxp.plugins.Zoom.prototype": {
+        zoomInMenuText: "Zoom In",
+        zoomOutMenuText: "Zoom Out",
+        zoomInTooltip: "Zoom In",
+        zoomOutTooltip: "Zoom Out"
+    },
+    
+    "gxp.plugins.ZoomToExtent.prototype": {
+        menuText: "Zoom To Max Extent",
+        tooltip: "Zoom To Max Extent"
+    },
+    
+    "gxp.plugins.ZoomToDataExtent.prototype": {
+        menuText: "Zoom to layer extent",
+        tooltip: "Zoom to layer extent"
+    },
+
+    "gxp.plugins.ZoomToLayerExtent.prototype": {
+        menuText: "Zoom to layer extent",
+        tooltip: "Zoom to layer extent"
+    },
+    
+    "gxp.plugins.ZoomToSelectedFeatures.prototype": {
+        menuText: "Zoom to selected features",
+        tooltip: "Zoom to selected features"
+    },
+
+    "gxp.FeatureEditPopup.prototype": {
+        closeMsgTitle: "Save Changes?",
+        closeMsg: "This feature has unsaved changes. Would you like to save your changes?",
+        deleteMsgTitle: "Delete Feature?",
+        deleteMsg: "Are you sure you want to delete this feature?",
+        editButtonText: "Edit",
+        editButtonTooltip: "Make this feature editable",
+        deleteButtonText: "Delete",
+        deleteButtonTooltip: "Delete this feature",
+        cancelButtonText: "Cancel",
+        cancelButtonTooltip: "Stop editing, discard changes",
+        saveButtonText: "Save",
+        saveButtonTooltip: "Save changes"
+    },
+    
+    "gxp.FillSymbolizer.prototype": {
+        fillText: "Fill",
+        colorText: "Color",
+        opacityText: "Opacity"
+    },
+    
+    "gxp.FilterBuilder.prototype": {
+        builderTypeNames: ["any", "all", "none", "not all"],
+        preComboText: "Match",
+        postComboText: "of the following:",
+        addConditionText: "add condition",
+        addGroupText: "add group",
+        removeConditionText: "remove condition"
+    },
+    
+    "gxp.grid.CapabilitiesGrid.prototype": {
+        nameHeaderText : "Name",
+        titleHeaderText : "Title",
+        queryableHeaderText : "Queryable",
+        layerSelectionLabel: "View available data from:",
+        layerAdditionLabel: "or add a new server.",
+        expanderTemplateText: "<p><b>Abstract:</b> {abstract}</p>"
+    },
+    
+    "gxp.PointSymbolizer.prototype": {
+        graphicCircleText: "circle",
+        graphicSquareText: "square",
+        graphicTriangleText: "triangle",
+        graphicStarText: "star",
+        graphicCrossText: "cross",
+        graphicXText: "x",
+        graphicExternalText: "external",
+        urlText: "URL",
+        opacityText: "opacity",
+        symbolText: "Symbol",
+        sizeText: "Size",
+        rotationText: "Rotation"
+    },
+
+    "gxp.QueryPanel.prototype": {
+        queryByLocationText: "Query by location",
+        currentTextText: "Current extent",
+        queryByAttributesText: "Query by attributes",
+        layerText: "Layer"
+    },
+    
+    "gxp.RulePanel.prototype": {
+        scaleSliderTemplate: "{scaleType} Scale 1:{scale}",
+        labelFeaturesText: "Label Features",
+        advancedText: "Advanced",
+        limitByScaleText: "Limit by scale",
+        limitByConditionText: "Limit by condition",
+        symbolText: "Symbol",
+        nameText: "Name"
+    },
+    
+    "gxp.ScaleLimitPanel.prototype": {
+        scaleSliderTemplate: "{scaleType} Scale 1:{scale}",
+        maxScaleLimitText: "Max scale limit"
+    },
+    
+    "gxp.TextSymbolizer.prototype": {
+        labelValuesText: "Label values",
+        haloText: "Halo",
+        sizeText: "Size"
+    },
+    
+    "gxp.WMSLayerPanel.prototype": {
+        aboutText: "About",
+        titleText: "Title",
+        nameText: "Name",
+        descriptionText: "Description",
+        displayText: "Display",
+        opacityText: "Opacity",
+        formatText: "Format",
+        transparentText: "Transparent",
+        cacheText: "Cache",
+        cacheFieldText: "Use cached version"
+    },
+
+    "gxp.EmbedMapDialog.prototype": {
+        publishMessage: "Your map is ready to be published to the web! Simply copy the following HTML to embed the map in your website:",
+        heightLabel: 'Height',
+        widthLabel: 'Width',
+        mapSizeLabel: 'Map Size',
+        miniSizeLabel: 'Mini',
+        smallSizeLabel: 'Small',
+        premiumSizeLabel: 'Premium',
+        largeSizeLabel: 'Large'
+    }
+
+});
+
