a239c50ffc76046a7683610a975d7c13fbeccd2d
tschaub <tim.schaub@gmail.com>
2011-02-14 09:46:39 -0700

    Doc correction.

--- src/script/plugins/WMSSource.js
+++ src/script/plugins/WMSSource.js
@@ -86,7 +86,7 @@ gxp.plugins.WMSSource = Ext.extend(gxp.plugins.LayerSource, {
      *  ``String`` WMS service URL for this source
      */
 
-    /** api: baseParams
+    /** api: config[baseParams]
      *  ``Object`` Base parameters to use on the WMS GetCapabilities
      *  request.
      */
