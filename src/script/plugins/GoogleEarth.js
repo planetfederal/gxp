792205ac2163eda9c12fc4b9891ba93e40b013fd
tschaub <tim.schaub@gmail.com>
2011-04-11 18:10:11 -0600

    Make a singleton loader instead of monitor.

--- src/script/plugins/GoogleEarth.js
+++ src/script/plugins/GoogleEarth.js
@@ -68,6 +68,14 @@ gxp.plugins.GoogleEarth = Ext.extend(gxp.plugins.Tool, {
     /** api: ptype = gxp_googleearth */
     ptype: "gxp_googleearth",
 
+    /** config: config[timeout]
+     *  ``Number``
+     *  The time (in milliseconds) to wait before giving up on the Google API
+     *  script loading.  This layer source will not be availble if the script
+     *  does not load within the given timeout.  Default is 7000 (seven seconds).
+     */
+    timeout: 7000,
+
     /** config: property[apiKey]
      *  ``String`` The API key required for adding the Google Maps script
      */
@@ -85,12 +93,12 @@ gxp.plugins.GoogleEarth = Ext.extend(gxp.plugins.Tool, {
 
     loadScript: function() {
 
-        if(!this.initialConfig.apiKey && window.location.hostname !== "localhost") {
+        if (!this.initialConfig.apiKey) {
             Ext.Msg.prompt("Google API Key",
                 this.apiKeyPrompt + window.location.hostname +
                     " <sup><a target='_blank' href='http://code.google.com/apis/earth/'>?</a></sup>",
                 function(btn, key) {
-                    if(btn === "ok") {
+                    if (btn === "ok") {
                         this.initialConfig.apiKey = key;
                         this.loadScript();
                     } else {
@@ -101,20 +109,14 @@ gxp.plugins.GoogleEarth = Ext.extend(gxp.plugins.Tool, {
             return;
         }
         
-        var params = {
-            key: this.initialConfig.apiKey,
-            autoload: Ext.encode({
-                modules: [{
-                    name: "earth",
-                    version: "1",
-                    callback: "gxp.plugins.GoogleEarth.monitor.onScriptLoad"
-                }]
-            })
-        };
-        
-        var script = document.createElement("script");
-        script.src = "http://www.google.com/jsapi?" + Ext.urlEncode(params);
-        document.getElementsByTagName("head")[0].appendChild(script);
+        gxp.plugins.GoogleEarth.loader.onLoad({
+            apiKey: this.initialConfig.apiKey,
+            callback: function() {
+                this.actions[0].enable();
+            },
+            // TODO: add errback
+            scope: this
+        });
 
     },
 
@@ -141,28 +143,29 @@ gxp.plugins.GoogleEarth = Ext.extend(gxp.plugins.Tool, {
             scope: this
         }];
         var result = gxp.plugins.GoogleEarth.superclass.addActions.apply(this, [actions]);
-        if (gxp.plugins.GoogleEarth.monitor.ready) {
+        if (gxp.plugins.GoogleEarth.loader.ready) {
             this.actions[0].enable();
         } else {
-            gxp.plugins.GoogleEarth.monitor.on({
+            gxp.plugins.GoogleEarth.loader.on({
                 ready: function() {
                     this.actions[0].enable();
                 },
                 scope: this
             });
-            if (!gxp.plugins.GoogleEarth.monitor.loading) {
+            if (!gxp.plugins.GoogleEarth.loader.loading) {
                 this.loadScript();
             }
         }
         return result;
     }
-        
+
 });
 
+
 /**
- * Create a monitor singleton that all plugin instances can use.
+ * Create a loader singleton that all plugin instances can use.
  */
-gxp.plugins.GoogleEarth.monitor = new (Ext.extend(Ext.util.Observable, {
+gxp.plugins.GoogleEarth.loader = new (Ext.extend(Ext.util.Observable, {
 
     /** private: property[ready]
      *  ``Boolean``
@@ -181,8 +184,14 @@ gxp.plugins.GoogleEarth.monitor = new (Ext.extend(Ext.util.Observable, {
             /** private: event[ready]
              *  Fires when this plugin type is ready.
              */
-             "ready"
+             "ready",
+
+             /** private: event[failure]
+              *  Fires when script loading fails.
+              */
+              "failure"
         );
+        return Ext.util.Observable.prototype.constructor.apply(this, arguments);
     },
     
     /** private: method[onScriptLoad]
@@ -190,12 +199,85 @@ gxp.plugins.GoogleEarth.monitor = new (Ext.extend(Ext.util.Observable, {
      */
     onScriptLoad: function() {
         // the google loader calls this in the window scope
-        var monitor = gxp.plugins.GoogleEarth.monitor;
+        var monitor = gxp.plugins.GoogleEarth.loader;
         if (!monitor.ready) {
             monitor.ready = true;
             monitor.loading = false;
             monitor.fireEvent("ready");
         }
+    },
+    
+    /** api: method[gxp.plugins.GoogleEarth.loader.onLoad]
+     *  :arg options: ``Object``
+     *
+     *  Options:
+     *
+     *  * apiKey - ``String`` API key for Google Earth plugin.
+     *  * callback - ``Function`` Called when script loads.
+     *  * errback - ``Function`` Called if loading fails.
+     *  * timeout - ``Number`` Time to wait before deciding that loading failed
+     *      (in milliseconds).
+     *  * scope - ``Object`` The ``this`` object for callbacks.
+     */
+    onLoad: function(options) {
+        if (this.ready) {
+            // call this in the next turn for consistent return before callback
+            window.setTimeout(function() {
+                options.callback.call(options.scope);                
+            }, 0);
+        } else if (!this.loading) {
+            this.loadScript(options);
+        } else {
+            this.on({
+                ready: options.callback,
+                failure: options.errback || Ext.emptyFn,
+                scope: options.scope
+            });
+        }
+    },
+
+    /** private: method[onScriptLoad]
+     *  Called when all resources required by this plugin type have loaded.
+     */
+    loadScript: function(options) {
+
+        var params = {
+            key: options.apiKey,
+            autoload: Ext.encode({
+                modules: [{
+                    name: "earth",
+                    version: "1",
+                    callback: "gxp.plugins.GoogleEarth.loader.onScriptLoad"
+                }]
+            })
+        };
+        
+        var script = document.createElement("script");
+        script.src = "http://www.google.com/jsapi?" + Ext.urlEncode(params);
+
+        // cancel loading if monitor is not ready within timeout
+        var errback = options.errback || Ext.emptyFn;
+        var timeout = options.timeout || gxp.plugins.GoogleSource.prototype.timeout;
+        window.setTimeout((function() {
+            if (!gxp.plugins.GoogleEarth.loader.ready) {
+                this.loading = false;
+                this.ready = false;
+                document.getElementsByTagName("head")[0].removeChild(script);
+                errback.call(options.scope);
+                this.fireEvent("failure");
+                this.purgeListeners();
+            }
+        }).createDelegate(this), timeout);
+        
+        // register callback for ready
+        this.on({
+            ready: options.callback,
+            scope: options.scope
+        });
+
+        this.loading = true;
+        document.getElementsByTagName("head")[0].appendChild(script);
+
     }
 
 }))();
