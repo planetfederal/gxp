370eeaa655e63a62d710e7368b1a7c624da4ea4c
Matt Priour <mpriour@gmail.com>
2011-10-07 11:52:39 -0400

    Add proper destroy method and destroy time manager control when there are no time layers

--- src/script/widgets/PlaybackPanel.js
+++ src/script/widgets/PlaybackPanel.js
@@ -93,6 +93,18 @@ gxp.PlaybackPanel = Ext.extend(Ext.Panel, {
         })
         gxp.PlaybackPanel.superclass.initComponent.call(this);       
     },
+    /** private: method[destroy]
+     *  Destory the component.
+     */
+    destroy: function(){
+        //kill the control but only if we created the control
+        if(this.control && !this.initialConfig.control){
+            this.control.map && this.control.map.removeControl(this.control);
+            this.control.destroy()
+            this.control = null;
+        }
+        gxp.PlaybackPanel.superclass.destroy.call(this);
+    },
     /** private: method[buildPlaybackItems] */
     buildPlaybackItems: function(){
         if (this.control.timeAgents) {
@@ -296,21 +308,31 @@ gxp.PlaybackPanel = Ext.extend(Ext.Panel, {
         var ctl = this.control;
         ctl.guessPlaybackRate();
         ctl.events.register("rangemodified", this, function(){
-            var oldvals = {
-                start: ctl.range[0].getTime(),
-                end: ctl.range[1].getTime(),
-                resolution: {
-                    units: ctl.units,
-                    step: ctl.step
-                }
+            if (!ctl.timeAgents || !ctl.timeAgents.length) {
+                //we don't have any time agents which means we should get rid of the time manager control
+                //we will automattically add the control back when a time layer is added via handlers on the
+                //playback plugin or the application code if the playback toolbar was not build via the plugin
+                ctl.map.removeControl(this.ctl);
+                ctl.destroy();
+                ctl = null;
             }
-            ctl.guessPlaybackRate();
-            if (ctl.range[0].getTime() != oldvals.start || ctl.range[1].getTime() != oldvals.end || ctl.units != oldvals.units || ctl.step != oldvals.step) {
-                this.reconfigureSlider(this.buildSliderValues());
-                if (this.playbackMode == 'ranged' || this.playbackMode == 'decay') {
-                    this.control.incrementTime(this.control.rangeInterval, this.control.units)
+            else {
+                var oldvals = {
+                    start: ctl.range[0].getTime(),
+                    end: ctl.range[1].getTime(),
+                    resolution: {
+                        units: ctl.units,
+                        step: ctl.step
+                    }
                 }
-                this.setThumbStyles(this.slider)
+                ctl.guessPlaybackRate();
+                if (ctl.range[0].getTime() != oldvals.start || ctl.range[1].getTime() != oldvals.end || ctl.units != oldvals.units || ctl.step != oldvals.step) {
+                    this.reconfigureSlider(this.buildSliderValues());
+                    if (this.playbackMode == 'ranged' || this.playbackMode == 'decay') {
+                        this.control.incrementTime(this.control.rangeInterval, this.control.units)
+                    }
+                    this.setThumbStyles(this.slider)
+                }
             }
         })
     },
