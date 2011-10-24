3f058e88934457ce0a9a4d6e486c9b4af00a6ff0
Matt Priour <mpriour@gmail.com>
2011-10-24 10:08:36 -0500

    Corrected syntax for setting the slider's thumb value in the setTime function

--- src/script/widgets/PlaybackToolbar.js
+++ src/script/widgets/PlaybackToolbar.js
@@ -170,7 +170,7 @@ gxp.PlaybackToolbar = Ext.extend(Ext.Toolbar, {
         if(timeVal<this.slider.minValue || timeVal>this.slider.maxValue){
             return false;
         }else{
-            this.slider.setValue(timeVal);
+            this.slider.setValue(0,timeVal);
             return true;
         }
     },
