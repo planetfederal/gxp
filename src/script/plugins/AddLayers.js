planetfederal/gxp

commit 9c44bb12a2d6d6ccf808015c762423e9bd8db75e

when switching GE the upload button might be visible for Google source (closes http://projects.opengeo.org/suite/ticket/1365)

src/script/plugins/AddLayers.js
@@ -401,7 +401,9 @@ gxp.plugins.AddLayers = Ext.extend(gxp.plugins.Tool, {
                     capGridPanel.getSelectionModel().clearSelections();
                 },
                 show: function(win) {
-                    this.setSelectedSource(this.target.layerSources[data[idx][0]]);
+                    if (this.selectedSource === null) {
+                        this.setSelectedSource(this.target.layerSources[data[idx][0]]);
+                    }
                 },
                 scope: this
             }

commit 516413f9c5c60f498ab555884f4294a1a4cd3a4d

fix bug introduced with 9c44bb12a2d6d6ccf808015c762423e9bd8db75e

src/script/plugins/AddLayers.js
@@ -404,6 +404,8 @@ gxp.plugins.AddLayers = Ext.extend(gxp.plugins.Tool, {
                 show: function(win) {
                     if (this.selectedSource === null) {
                         this.setSelectedSource(this.target.layerSources[data[idx][0]]);
+                    } else {
+                        this.setSelectedSource(this.selectedSource);
                     }
                 },
                 scope: this