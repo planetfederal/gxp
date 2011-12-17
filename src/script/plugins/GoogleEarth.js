99c8a3b38197958490efe2bd6694714cd0a459b9
ahocevar <andreas.hocevar@gmail.com>
2011-12-17 10:31:57 +0100

    Don't add the google loader script too early.
    The google loader appends elements to document.body, which fails if document.body is not yet available. So we wait with appending the loader script until document.body is available.

--- src/script/plugins/GoogleEarth.js
+++ src/script/plugins/GoogleEarth.js
@@ -369,7 +369,18 @@ gxp.plugins.GoogleEarth.loader = new (Ext.extend(Ext.util.Observable, {
         });
 
         this.loading = true;
-        document.getElementsByTagName("head")[0].appendChild(script);
+        
+        // The goodle loader accesses document.body, so we don't add the loader
+        // script before the document is ready.
+        function append() {
+            document.getElementsByTagName("head")[0].appendChild(script);
+        }
+        if (document.body) {
+            append();
+        } else {
+            Ext.onReady(append);
+        }
+        
         this.script = script;
 
     },
