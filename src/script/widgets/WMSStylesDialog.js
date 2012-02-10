eb264bf635d4048c304383d212f5d76907b8143e
Bart van den Eijnden <bartvde@opengeo.org>
2012-02-10 13:16:58 +0100

    add more missing dependencies to WMSStylesDialog

--- src/script/widgets/WMSStylesDialog.js
+++ src/script/widgets/WMSStylesDialog.js
@@ -11,6 +11,8 @@
  * @require widgets/RulePanel.js
  * @require widgets/StylePropertiesDialog.js
  * @require OpenLayers/Renderer.js
+ * @require OpenLayers/Style2.js
+ * @require OpenLayers/Format/SLD/v1_0_0.js
  * @require GeoExt/data/AttributeStore.js
  * @require GeoExt/widgets/WMSLegend.js
  */
