2e33a9d1a06eb63bca30940127864b7b6209f361
Bart van den Eijnden <bartvde@opengeo.org>
2012-02-28 12:12:52 +0100

    add background color and number of zoom levels

--- src/script/plugins/MapProperties.js
+++ src/script/plugins/MapProperties.js
@@ -8,6 +8,7 @@
 
 /**
  * @requires plugins/Tool.js
+ * @requires widgets/form/ColorField.js
  */
 
 /** api: (define)
@@ -30,6 +31,13 @@ gxp.plugins.MapProperties = Ext.extend(gxp.plugins.Tool, {
     /** api: ptype = gxp_mapproperties */
     ptype: "gxp_mapproperties",
 
+    /** api: config[colorManager]
+     *  ``Function``
+     *  Optional color manager constructor to be used as a plugin for the color
+     *  field.
+     */
+    colorManager: null,
+
     /** api: config[menuText]
      *  ``String``
      *  Text for map properties menu item (i18n).
@@ -41,6 +49,11 @@ gxp.plugins.MapProperties = Ext.extend(gxp.plugins.Tool, {
      *  Text for map properties action tooltip (i18n).
      */
     toolTip: "Map Properties",
+
+    /* i18n */
+    wrapDateLineText: "Wrap dateline",
+    numberOfZoomLevelsText: "Number of zoom levels",
+    colorText: "Background color",
 
     addActions: function() {
         return gxp.plugins.MapProperties.superclass.addActions.apply(this, [{
@@ -56,20 +69,49 @@ gxp.plugins.MapProperties = Ext.extend(gxp.plugins.Tool, {
     },
 
     addOutput: function() {
+        var colorFieldPlugins;
+        if (this.colorManager) {
+            colorFieldPlugins = [new this.colorManager()];
+        }
         return gxp.plugins.MapProperties.superclass.addOutput.call(this, {
             xtype: 'form',
             border: false,
             bodyStyle: "padding: 10px",
             items: [{
+                xtype: 'numberfield',
+                allowNegative: false,
+                allowDecimals: false,
+                fieldLabel: this.numberOfZoomLevelsText,
+                minValue: 1,
+                value: this.target.mapPanel.map.baseLayer.numZoomLevels,
+                listeners: {
+                    "change": function(fld, value) {
+                        this.target.mapPanel.map.baseLayer.addOptions({numZoomLevels: value});
+                    },
+                    scope: this
+                }
+            }, {
                 xtype: 'checkbox',
-                fieldLabel: "Wrap dateline",
-                value: this.target.mapPanel.map.baseLayer.wrapDateLine,
+                fieldLabel: this.wrapDateLineText,
+                checked: this.target.mapPanel.map.baseLayer.wrapDateLine,
                 listeners: {
                     "check": function(cb, value) {
                         this.target.mapPanel.map.baseLayer.wrapDateLine = value;
                     },
                     scope: this
                 }
+            }, {
+                xtype: "gxp_colorfield",
+                fieldLabel: this.colorText,
+                value: this.target.mapPanel.body.getColor('background-color'),
+                plugins: colorFieldPlugins,
+                listeners: {
+                    valid: function(field) {
+                        this.target.mapPanel.body.setStyle('background-color', field.getValue());
+                    },
+                    scope: this
+                }
+                
             }]
         });
     }
