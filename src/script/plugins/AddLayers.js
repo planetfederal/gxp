planetfederal/gxp

commit ce815ca0a0eb410f4470b60997a46d99f621bb0e

prevent empty window from addlayers dialog see https://github.com/opengeo/mapstory/issues/283 for more info

src/script/plugins/AddLayers.js
@@ -261,8 +261,6 @@ gxp.plugins.AddLayers = Ext.extend(gxp.plugins.Tool, {
     showCapabilitiesGrid: function() {
         if(!this.capGrid) {
             this.initCapGrid();
-        } else {
-            this.addOutput(this.capGrid);
         }
         this.capGrid.show();
     },

commit 69e5377552107fc8074b2a6acc35a88a8e52ca80

fix the breakage of the crumb navigation introduced with https://github.com/opengeo/gxp/commit/ce815ca0a0eb410f4470b60997a46d99f621bb0e thanks @ahocevar for providing the fix

src/script/plugins/AddLayers.js
@@ -261,6 +261,8 @@ gxp.plugins.AddLayers = Ext.extend(gxp.plugins.Tool, {
     showCapabilitiesGrid: function() {
         if(!this.capGrid) {
             this.initCapGrid();
+        } else if (!(this.capGrid instanceof Ext.Window)) {
+            this.addOutput(this.capGrid);
         }
         this.capGrid.show();
     },