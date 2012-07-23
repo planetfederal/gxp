60a76b23b564d1a01ee6f7c7fc7758a39752e7e7
Bart van den Eijnden <bartvde@opengeo.org>
2012-07-23 14:21:00 +0200

    setting a value of null should not result in an error

--- src/script/widgets/form/ExtendedDateField.js
+++ src/script/widgets/form/ExtendedDateField.js
@@ -102,15 +102,17 @@ gxp.form.ExtendedDateField = Ext.extend(Ext.form.DateField, {
             d = new Date(parseFloat(v)*1000);
         }
         var str = this.formatDate(d);
-        var bcYear = str.match(this.bcYrRegEx);
-        if (bcYear) {
-            bcYear = bcYear[0] || bcYear[1];
-            if (bcYear && bcYear.length < 5) {
-                var zeropad = '-';
-                for (var i=bcYear.length;i<=4; ++i) {
-                    zeropad += '0';
+        if (str) {
+            var bcYear = str.match(this.bcYrRegEx);
+            if (bcYear) {
+                bcYear = bcYear[0] || bcYear[1];
+                if (bcYear && bcYear.length < 5) {
+                    var zeropad = '-';
+                    for (var i=bcYear.length;i<=4; ++i) {
+                        zeropad += '0';
+                    }
+                    str = str.replace(bcYear, zeropad + Math.abs(parseInt(bcYear, 10)));
                 }
-                str = str.replace(bcYear, zeropad + Math.abs(parseInt(bcYear, 10)));
             }
         }
         return Ext.form.DateField.superclass.setValue.call(this, str);
