a4e1df6834d140b33199a1e2162c1266b3f95aa9
Martins Linde <martins.linde@gmail.com>
2012-08-24 18:17:21 +0300

    added Latvian translations

---
+++ src/script/locale/lv.js
@@ -0,0 +1,391 @@
+/**
+ * @requires GeoExt/Lang.js
+ */
+
+GeoExt.Lang.add("lv", {
+
+    "gxp.menu.LayerMenu.prototype": {
+        layerText: "Slānis"
+    },
+
+    "gxp.plugins.AddLayers.prototype": {
+        addActionMenuText: "Pievienot slāņus",
+        addActionTip: "Pievienot slāņus",
+        addServerText: "Pievienot jaunu WMS serveri",
+        addButtonText: "Pievienot slāņus",
+        untitledText: "Bez nosaukuma",
+        addLayerSourceErrorText: "Neizdevās iegūt WMS aprakstu ({msg}).\nPārbaudiet ievadīto adresi.",
+        availableLayersText: "Pieejamie slāņi",
+        expanderTemplateText: "<p><b>Apraksts:</b> {abstract}</p>",
+        panelTitleText: "Nosaukums",
+        layerSelectionText: "Apskatīt pieejamos datus no:",
+        doneText: "Labi",
+        uploadText: "Augšupielādēt slāņus"
+    },
+    
+    "gxp.plugins.BingSource.prototype": {
+        title: "Bing slāņi",
+        roadTitle: "Ceļu karte (Bing Roads)",
+        aerialTitle: "Satelītkarte (Bing Aerial)",
+        labeledAerialTitle: "Satelītkarte ar uzrakstiem (Bing Aerial With Labels)"
+    },
+
+    "gxp.plugins.FeatureEditor.prototype": {
+        splitButtonText: "Rediģēt",
+        createFeatureActionText: "Izveidot jaunu",
+        editFeatureActionText: "Rediģēt esošu",
+        createFeatureActionTip: "Veidot jaunu objektu",
+        editFeatureActionTip: "Rediģēt esošu objektu"
+    },
+    
+    "gxp.plugins.FeatureGrid.prototype": {
+        displayFeatureText: "Parādīt uz kartes",
+        firstPageTip: "Pirmā lapa",
+        previousPageTip: "Iepriekšējā lapa",
+        zoomPageExtentTip: "Pietuvināt karti pēc objektiem",
+        nextPageTip: "Nākamā lapa",
+        lastPageTip: "Pēdējā lapa",
+        totalMsg: "Rāda no {1} līdz {2} objektus (kopā {0})"
+    },
+
+    "gxp.plugins.GoogleEarth.prototype": {
+        menuText: "3D Skats",
+        tooltip: "Pārslēgties uz 3D skatu"
+    },
+    
+    "gxp.plugins.GoogleSource.prototype": {
+        title: "Google slāņi",
+        roadmapAbstract: "Ielu karte",
+        satelliteAbstract: "Satelītkarte",
+        hybridAbstract: "Satelītkarte ar uzrakstiem",
+        terrainAbstract: "Ielu karte ar reljefu"
+    },
+
+    "gxp.plugins.LayerProperties.prototype": {
+        menuText: "Slāņa iestatījumi",
+        toolTip: "Slāņa iestatījumi"
+    },
+    
+    "gxp.plugins.LayerTree.prototype": {
+        shortTitle: "Slāņi",
+        rootNodeText: "Slāņi",
+        overlayNodeText: "Virsslāņi",
+        baseNodeText: "Fona slāņi"
+    },
+
+    "gxp.plugins.Legend.prototype": {
+        menuText: "Kartes apzīmējumi",
+        tooltip: "Parādīt kartes apzīmējumus"
+    },
+
+    "gxp.plugins.LoadingIndicator.prototype": {
+        loadingMapMessage: "Ielādē karti..."
+    },
+
+    "gxp.plugins.MapBoxSource.prototype": {
+        title: "MapBox slāņi",
+        blueMarbleTopoBathyJanTitle: "Blue Marble Topography & Bathymetry (January)",
+        blueMarbleTopoBathyJulTitle: "Blue Marble Topography & Bathymetry (July)",
+        blueMarbleTopoJanTitle: "Blue Marble Topography (January)",
+        blueMarbleTopoJulTitle: "Blue Marble Topography (July)",
+        controlRoomTitle: "Control Room",
+        geographyClassTitle: "Geography Class",
+        naturalEarthHypsoTitle: "Natural Earth Hypsometric",
+        naturalEarthHypsoBathyTitle: "Natural Earth Hypsometric & Bathymetry",
+        naturalEarth1Title: "Natural Earth I",
+        naturalEarth2Title: "Natural Earth II",
+        worldDarkTitle: "World Dark",
+        worldLightTitle: "World Light",
+        worldPrintTitle: "World Print"
+    },
+
+    "gxp.plugins.Measure.prototype": {
+        buttonText: "Mērīšana",
+        lengthMenuText: "Garums",
+        areaMenuText: "Platība",
+        lengthTooltip: "Garums",
+        areaTooltip: "Platība",
+        measureTooltip: "Mērīt"
+    },
+
+    "gxp.plugins.Navigation.prototype": {
+        menuText: "Pārvietot karti",
+        tooltip: "Pārvietot karti"
+    },
+
+    "gxp.plugins.NavigationHistory.prototype": {
+        previousMenuText: "Kartes iepriekšējais skatījums",
+        nextMenuText: "Kartes nākamais skatījums",
+        previousTooltip: "Kartes iepriekšējais skatījums",
+        nextTooltip: "Kartes nākamais skatījums"
+    },
+
+    "gxp.plugins.OSMSource.prototype": {
+        title: "OpenStreetMap slāņi",
+        mapnikAttribution: "Dati CC-By-SA no <a href='http://openstreetmap.org/'>OpenStreetMap</a>",
+        osmarenderAttribution: "Dati CC-By-SA no <a href='http://openstreetmap.org/'>OpenStreetMap</a>"
+    },
+
+    "gxp.plugins.Print.prototype": {
+        buttonText:"Drukāt",
+        menuText: "Drukāt karti",
+        tooltip: "Drukāt karti",
+        previewText: "Priekšskatījums",
+        notAllNotPrintableText: "Dažus slāņus nav iespējams drukāt",
+        nonePrintableText: "Nevienu no aktīvajiem slāņiem nevar drukāt"
+    },
+
+    "gxp.plugins.MapQuestSource.prototype": {
+        title: "MapQuest slāņi",
+        osmAttribution: "Karte Courtesy no <a href='http://open.mapquest.co.uk/' target='_blank'>MapQuest</a> <img src='http://developer.mapquest.com/content/osm/mq_logo.png' border='0'>",
+        osmTitle: "MapQuest OpenStreetMap karte",
+        naipAttribution: "Karte Courtesy of <a href='http://open.mapquest.co.uk/' target='_blank'>MapQuest</a> <img src='http://developer.mapquest.com/content/osm/mq_logo.png' border='0'>",
+        naipTitle: "MapQuest satelītkarte"
+    },
+
+    "gxp.plugins.QueryForm.prototype": {
+        queryActionText: "Meklēt",
+        queryMenuText: "Meklēt slānī datus",
+        queryActionTip: "Meklēt atzīmētajā slānī",
+        queryByLocationText: "Meklēt redzamajā kartes daļā",
+        queryByAttributesText: "Meklēt pēc atribūtiem",
+        queryMsg: "Meklē...",
+        cancelButtonText: "Atcelt",
+        noFeaturesTitle: "Netika atrasts",
+        noFeaturesMessage: "Netika atrasts neviens objekts."
+    },
+
+    "gxp.plugins.RemoveLayer.prototype": {
+        removeMenuText: "Noņemt slāni",
+        removeActionTip: "Noņemt slāni"
+    },
+    
+    "gxp.plugins.Styler.prototype": {
+        menuText: "Labot apzīmējumus",
+        tooltip: "Labot slāņa apzīmējumus"
+
+    },
+
+    "gxp.plugins.WMSGetFeatureInfo.prototype": {
+        buttonText:"Identificēt",
+        infoActionTip: "Informācija par kartes objektiem",
+        popupTitle: "Objektu informācija"
+    },
+
+    "gxp.plugins.Zoom.prototype": {
+        zoomMenuText: "Tuvināšana",
+        zoomInMenuText: "Pietuvināt",
+        zoomOutMenuText: "Attālināt",
+        zoomTooltip: "Pietuvināt iezīmētajā taisnstūrī",
+        zoomInTooltip: "Pietuvināt",
+        zoomOutTooltip: "Attālināt"
+    },
+    
+    "gxp.plugins.ZoomToExtent.prototype": {
+        menuText: "Kartes kopskats",
+        tooltip: "Kartes kopskats"
+    },
+    
+    "gxp.plugins.ZoomToDataExtent.prototype": {
+        menuText: "Atlasīto objektu kopskats",
+        tooltip: "Atlasīto objektu kopskats"
+    },
+
+    "gxp.plugins.ZoomToLayerExtent.prototype": {
+        menuText: "Slāņa kopskats",
+        tooltip: "Slāņa kopskats"
+    },
+    
+    "gxp.plugins.ZoomToSelectedFeatures.prototype": {
+        menuText: "Pietuvināt atzīmētos objektus",
+        tooltip: "Pietuvināt atzīmētos objektus"
+    },
+
+    "gxp.FeatureEditPopup.prototype": {
+        closeMsgTitle: "Saglabāt izmaiņas?",
+        closeMsg: "Objektam ir izdarītas izmaiņas. Vai saglabāt?",
+        deleteMsgTitle: "Dzēst objektu?",
+        deleteMsg: "Vai esat drošs, ka vēlaties dzēst objektu?",
+        editButtonText: "Labot",
+        editButtonTooltip: "Labot objektu",
+        deleteButtonText: "Dzēst",
+        deleteButtonTooltip: "Dzēst objektu",
+        cancelButtonText: "Atcelt",
+        cancelButtonTooltip: "Pārtraukt labošanu un atcelt izmaiņas",
+        saveButtonText: "Saglabāt",
+        saveButtonTooltip: "Saglabāt izmaiņas"
+    },
+    
+    "gxp.FillSymbolizer.prototype": {
+        fillText: "Aizpildījums",
+        colorText: "Krāsa",
+        opacityText: "Caurspīdīgums"
+    },
+    
+    "gxp.FilterBuilder.prototype": {
+        builderTypeNames: ["jebkuram", "visiem", "nevienam", "ne visiem"],
+        preComboText: "Atbilst",
+        postComboText: "no nosacījumiem:",
+        addConditionText: "pievienot nosacījumu",
+        addGroupText: "pievienot nosacījumu grupu",
+        removeConditionText: "noņemt nosacījumu"
+    },
+    
+    "gxp.grid.CapabilitiesGrid.prototype": {
+        nameHeaderText : "Slānis",
+        titleHeaderText : "Nosaukums",
+        queryableHeaderText : "Meklēšanas iespēja",
+        layerSelectionLabel: "Pieejamie dati no:",
+        layerAdditionLabel: "vai pievienot jaunu serveri.",
+        expanderTemplateText: "<p><b>Apraksts:</b> {abstract}</p>"
+    },
+    
+    "gxp.PointSymbolizer.prototype": {
+        graphicCircleText: "aplis",
+        graphicSquareText: "kvadrāts",
+        graphicTriangleText: "trīsstūris",
+        graphicStarText: "zvaigzne",
+        graphicCrossText: "krusts",
+        graphicXText: "x",
+        graphicExternalText: "internetā",
+        urlText: "adrese (URL)",
+        opacityText: "caurspīdīgums",
+        symbolText: "Simbols",
+        sizeText: "Izmērs",
+        rotationText: "Rotācija"
+    },
+
+    "gxp.QueryPanel.prototype": {
+        queryByLocationText: "Meklēt kartes skatā",
+        currentTextText: "Kartes skats",
+        queryByAttributesText: "Meklēt pēc atribūtiem",
+        layerText: "Slānis"
+    },
+    
+    "gxp.RulePanel.prototype": {
+        scaleSliderTemplate: "{scaleType} Mērogs 1:{scale}",
+        labelFeaturesText: "Uzraksti",
+        labelsText: "Uzraksti",
+        basicText: "Vienkāršs teksts",
+        advancedText: "Papildiespējas",
+        limitByScaleText: "Ierobežot pēc mēroga",
+        limitByConditionText: "Ierobežot pēc nosacījuma",
+        symbolText: "Simbols",
+        nameText: "Nosaukums"
+    },
+    
+    "gxp.ScaleLimitPanel.prototype": {
+        scaleSliderTemplate: "{scaleType} Mērogs 1:{scale}",
+        minScaleLimitText: "Minimālais mērogs (tuvākais skats)",
+        maxScaleLimitText: "Maksimālais mērogs (tālākais skats)"
+    },
+    
+    "gxp.StrokeSymbolizer.prototype": {
+        solidStrokeName: "parasts",
+        dashStrokeName: "raustīts",
+        dotStrokeName: "punktots",
+        titleText: "Līnija",
+        styleText: "Veids",
+        colorText: "Krāsa",
+        widthText: "Biezums",
+        opacityText: "Caurspīdīgums"
+    },
+    
+    "gxp.StylePropertiesDialog.prototype": {   
+        titleText: "Vispārīgi",
+        nameFieldText: "Stils",
+        titleFieldText: "Nosaukums",
+        abstractFieldText: "Apraksts"
+    },
+    
+    "gxp.TextSymbolizer.prototype": {
+        labelValuesText: "Uzraksti",
+        haloText: "Kontūra",
+        sizeText: "Izmērs"
+    },
+    
+    "gxp.WMSLayerPanel.prototype": {
+        aboutText: "Par..",
+        titleText: "Nosaukums",
+        nameText: "Slānis",
+        descriptionText: "Apraksts",
+        displayText: "Izskats",
+        opacityText: "Caurspīdīgums",
+        formatText: "Formāts",
+        transparentText: "Caurspīdīgs",
+        cacheText: "Kešotā karte",
+        cacheFieldText: "Lietot kešoto karti",
+        stylesText: "Apzīmējumi",
+        infoFormatText: "Informācijas formāts",
+        infoFormatEmptyText: "Izvēlieties formātu"
+    },
+
+    "gxp.EmbedMapDialog.prototype": {
+        publishMessage: "Jūsu karte ir gatava publicēšanai internetā! Vienkārši iekopējiet HTML fragmentu mājas lapā:",
+        heightLabel: 'Augstums',
+        widthLabel: 'Platums',
+        mapSizeLabel: 'Kartes izmērs',
+        miniSizeLabel: 'Mini',
+        smallSizeLabel: 'Maza',
+        premiumSizeLabel: 'Liela',
+        largeSizeLabel: 'Vidēja'
+    },
+    
+    "gxp.WMSStylesDialog.prototype": {
+         addStyleText: "Pievienot",
+         addStyleTip: "Pievienot jaunus apzīmējumus",
+         chooseStyleText: "Izvēlēties apzīmējumus",
+         deleteStyleText: "Dzēst",
+         deleteStyleTip: "Dzēst iezīmētos apzīmējumus",
+         editStyleText: "Labot",
+         editStyleTip: "Labot iezīmētos apzīmējumus",
+         duplicateStyleText: "Kopēt",
+         duplicateStyleTip: "Kopēt iezimētos apzīmējumus",
+         addRuleText: "Pievienot",
+         addRuleTip: "Pievienot jaunu apzīmējumu",
+         newRuleText: "Jauns apzīmējums",
+         deleteRuleText: "Dzēst",
+         deleteRuleTip: "Dzēst iezīmēto apzīmējumu",
+         editRuleText: "Labot",
+         editRuleTip: "Labot iezīmēto apzīmējumu",
+         duplicateRuleText: "Kopēt",
+         duplicateRuleTip: "Kopēt iezīmēto apzīmējumu",
+         cancelText: "Atcelt",
+         saveText: "Saglabāt",
+         styleWindowTitle: "Apzīmējumi: {0}",
+         ruleWindowTitle: "Apzīmējuma nosacījums: {0}",
+         stylesFieldsetTitle: "Apzīmējumi",
+         rulesFieldsetTitle: "Apzīmējuma nosacījumi"
+    },
+
+    "gxp.LayerUploadPanel.prototype": {
+        titleLabel: "Nosaukums",
+        titleEmptyText: "Slāņa nosaukums",
+        abstractLabel: "Apraksts",
+        abstractEmptyText: "Slāņa apraksts",
+        fileLabel: "Dati",
+        fieldEmptyText: "Paņemt arhīvfailu...",
+        uploadText: "Augšupielādēt",
+        waitMsgText: "Augšupielādē jūsu datus...",
+        invalidFileExtensionText: "Failam jābūt vienam no: ",
+        optionsText: "Iespējas",
+        workspaceLabel: "Darba vide",
+        workspaceEmptyText: "Noklusētā darba vide",
+        dataStoreLabel: "Datu glabātuve",
+        dataStoreEmptyText: "Izveidot jaunu glabātuvi",
+        defaultDataStoreEmptyText: "Noklusētā glabātuve"
+    },
+    
+    "gxp.NewSourceDialog.prototype": {
+        title: "Pievienot jaunu serveri...",
+        cancelText: "Atcelt",
+        addServerText: "Pievienot serveri",
+        invalidURLText: "Ievadiet WMS servisa adresi (piemēram, http://example.com/geoserver/wms)",
+        contactingServerText: "Savienojas ar serveri..."
+    },
+
+    "gxp.ScaleOverlay.prototype": { 
+        zoomLevelText: "Tuvinājums"
+    }
+
+});
