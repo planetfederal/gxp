5ae545c81adeb5fd3b9fc62cb419c2aeadc0fdd3
Bart van den Eijnden <bartvde@opengeo.org>
2013-07-11 15:35:41 +0200

    make sure initial value of checkbox is correct in the case of yesno

--- src/script/widgets/TextSymbolizer.js
+++ src/script/widgets/TextSymbolizer.js
@@ -661,7 +661,7 @@ gxp.TextSymbolizer = Ext.extend(Ext.Panel, {
             xtype: "numberfield",
             allowNegative: false,
             value: this.symbolizer.vendorOptions[config.name],
-            checked: this.symbolizer.vendorOptions[config.name],
+            checked: (config.yesno === true) ? (this.symbolizer.vendorOptions[config.name] === 'yes') : this.symbolizer.vendorOptions[config.name],
             plugins: [{
                 ptype: 'gxp_formfieldhelp',
                 dismissDelay: 20000,
